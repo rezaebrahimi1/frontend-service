89d2758f114e1ca861ff93dab01b945d89c304e4
Ishita Sequeira <isequeir@redhat.com>
2021-05-11 17:57:43 -0400

    Update backend-url logic

--- MyAngularClient/src/app/products/products.service.ts
+++ MyAngularClient/src/app/products/products.service.ts
@@ -2,11 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product';
 
-// var host = process.env.COMPONENT_BACKEND_HOST || 'localhost';
-// var port = process.env.COMPONENT_BACKEND_PORT || 8081;
-
-const baseUrl = 'http://backend-app.stage.svc.cluster.local';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +9,7 @@ export class ProductsService {
 
   constructor(private http: HttpClient) {
   }
-
+  baseUrl = 'http://' + window.location.hostname.replace('frontend','backend');
   private async request(method: string, url: string, data?: any, responseType?: any) {
 
     console.log('request ' + JSON.stringify(data));
@@ -29,24 +24,24 @@ export class ProductsService {
   }
 
   getProducts() {
-    return this.request('get', `${baseUrl}/products`);
+    return this.request('get', `${this.baseUrl}/products`);
   }
 
   getProduct(id: string) {
-    return this.request('get', `${baseUrl}/product/${id}`);
+    return this.request('get', `${this.baseUrl}/product/${id}`);
   }
 
   createProduct(product: Product) {
     console.log('createProduct ' + JSON.stringify(product));
-    return this.request('post', `${baseUrl}/product`, product);
+    return this.request('post', `${this.baseUrl}/product`, product);
   }
 
   updateProduct(product: Product) {
     console.log('updateProduct ' + JSON.stringify(product));
-    return this.request('post', `${baseUrl}/product/${product.id}`, product);
+    return this.request('post', `${this.baseUrl}/product/${product.id}`, product);
   }
 
   deleteProduct(id: string) {
-    return this.request('delete', `${baseUrl}/product/${id}`, null, 'text');
+    return this.request('delete', `${this.baseUrl}/product/${id}`, null, 'text');
   }
 }
